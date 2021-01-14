0289bc13f9fb875ced4d36275519b36cd006c91f
Horo.BR <>
2021-01-14 15:56:11 +0300

    Scripts for 91 Days

---
+++ 91 Days/Vol 5/s04.bat
@@ -0,0 +1,1 @@
+x264-10bit-x64-2851 --level 5.0 --crf 16 --keyint 240 --deblock 1:-1  --bframes 9 --b-adapt 2 --ref 4 --deadzone-inter 21 --deadzone-intra 11 --qcomp 0.72 --aq-strength 0.85 --merange 32 --me umh --subme 9 --trellis 2 --direct spatial --no-mbtree --sar 1:1 --threads auto --colormatrix "bt709" --colorprim "bt709" --transfer "bt709" --output s04.264 --log-file s04.enc.log .\s04.avs
