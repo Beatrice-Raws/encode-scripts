b7fe273e1c7ec8d23fd692882e1294700b294f5e
Endill <brainvlad@gmail.com>
2021-02-20 23:08:57 +0300

    Add sources for Citrus

---
+++ Citrus/2.bat
@@ -0,0 +1,13 @@
+x264-x64-2901-aMod-avx.exe --output-depth 10 --level 5.0 --crf 14.7 --keyint 240 --deblock 1:-1  --bframes 9 --b-adapt 2 --ref 4 --deadzone-inter 21 --deadzone-intra 11 --qcomp 0.72 --aq-strength 0.85 --merange 32 --me umh --subme 9 --trellis 2 --direct spatial --no-mbtree --sar 1:1 --threads auto --colormatrix "bt709" --colorprim "bt709" --transfer "bt709" --output 01.264 --log-file 01.enc.log 01.vpy
+
+x264-x64-2901-aMod-avx.exe --output-depth 10 --level 5.0 --crf 14.7 --keyint 240 --deblock 1:-1  --bframes 9 --b-adapt 2 --ref 4 --deadzone-inter 21 --deadzone-intra 11 --qcomp 0.72 --aq-strength 0.85 --merange 32 --me umh --subme 9 --trellis 2 --direct spatial --no-mbtree --sar 1:1 --threads auto --colormatrix "bt709" --colorprim "bt709" --transfer "bt709" --output 04.264 --log-file 04.enc.log 04.vpy
+
+x264-x64-2901-aMod-avx.exe --output-depth 10 --level 5.0 --crf 14.7 --keyint 240 --deblock 1:-1  --bframes 9 --b-adapt 2 --ref 4 --deadzone-inter 21 --deadzone-intra 11 --qcomp 0.72 --aq-strength 0.85 --merange 32 --me umh --subme 9 --trellis 2 --direct spatial --no-mbtree --sar 1:1 --threads auto --colormatrix "bt709" --colorprim "bt709" --transfer "bt709" --output 05.264 --log-file 05.enc.log 05.vpy
+
+x264-x64-2901-aMod-avx.exe --output-depth 10 --level 5.0 --crf 14.7 --keyint 240 --deblock 1:-1  --bframes 9 --b-adapt 2 --ref 4 --deadzone-inter 21 --deadzone-intra 11 --qcomp 0.72 --aq-strength 0.85 --merange 32 --me umh --subme 9 --trellis 2 --direct spatial --no-mbtree --sar 1:1 --threads auto --colormatrix "bt709" --colorprim "bt709" --transfer "bt709" --output 06.264 --log-file 06.enc.log 06.vpy
+
+x264-x64-2901-aMod-avx.exe --output-depth 10 --level 5.0 --crf 14.7 --keyint 240 --deblock 1:-1  --bframes 9 --b-adapt 2 --ref 4 --deadzone-inter 21 --deadzone-intra 11 --qcomp 0.72 --aq-strength 0.85 --merange 32 --me umh --subme 9 --trellis 2 --direct spatial --no-mbtree --sar 1:1 --threads auto --colormatrix "bt709" --colorprim "bt709" --transfer "bt709" --output 02.264 --log-file 02.enc.log 02.vpy
+
+x264-x64-2901-aMod-avx.exe --output-depth 10 --level 5.0 --crf 14.7 --keyint 240 --deblock 1:-1  --bframes 9 --b-adapt 2 --ref 4 --deadzone-inter 21 --deadzone-intra 11 --qcomp 0.72 --aq-strength 0.85 --merange 32 --me umh --subme 9 --trellis 2 --direct spatial --no-mbtree --sar 1:1 --threads auto --colormatrix "bt709" --colorprim "bt709" --transfer "bt709" --output 03.264 --log-file 03.enc.log 03.vpy
+
+pause
