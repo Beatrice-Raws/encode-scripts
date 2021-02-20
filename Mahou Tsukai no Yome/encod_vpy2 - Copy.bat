37d2e169ea9209e9b83f6e363469b18bad5bf88f
Endill <brainvlad@gmail.com>
2021-02-20 23:19:02 +0300

    Add sources for Mahou Tsukai no Yome

---
+++ Mahou Tsukai no Yome/encod_vpy2 - Copy.bat
@@ -0,0 +1,53 @@
+.\vaporPortable-r44\vspipe.exe -y 16_720.vpy - | x265-x64-v3.0-aMod -F 16 --pools "" --hevc-aq --qp-adaptation-range 2 --limit-modes --open-gop --cbqpoffs -2 --crqpoffs -2 --no-rskip --no-tskip --keyint 240 --no-cutree --ref 6 --bframes 9 --bframe-bias 0 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 0.86 --deblock 1:-1 --tu-intra-depth 2 --tu-inter-depth 2 --me 2 --wpp --subme 5 --crf 13.7 --qcomp 0.72 --b-pyramid --merange 48 --weightp --weightb --rd 4 --psy-rd 2 --rdoq-level 2 --psy-rdoq 4 --sar 1:1 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "16_720.hevc" --csv-log-level 2 --csv "16_720.txt" --y4m -
+
+
+
+.\vaporPortable-r44\vspipe.exe -y 17.vpy - | x265-x64-v3.0-aMod -F 16 --pools "" --hevc-aq --qp-adaptation-range 2 --limit-modes --open-gop --cbqpoffs -2 --crqpoffs -2 --no-rskip --no-tskip --keyint 240 --no-cutree --ref 4 --bframes 9 --bframe-bias 0 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 0.86 --deblock 1:-1 --tu-intra-depth 2 --tu-inter-depth 2 --me 2 --wpp --subme 5 --crf 14.8 --qcomp 0.72 --b-pyramid --merange 48 --weightp --weightb --rd 4 --psy-rd 2 --rdoq-level 2 --psy-rdoq 4 --sar 1:1 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "17.hevc" --csv-log-level 2 --csv "17.txt" --y4m -
+
+.\vaporPortable-r44\vspipe.exe -y 17_720.vpy - | x265-x64-v3.0-aMod -F 16 --pools "" --hevc-aq --qp-adaptation-range 2 --limit-modes --open-gop --cbqpoffs -2 --crqpoffs -2 --no-rskip --no-tskip --keyint 240 --no-cutree --ref 6 --bframes 9 --bframe-bias 0 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 0.86 --deblock 1:-1 --tu-intra-depth 2 --tu-inter-depth 2 --me 2 --wpp --subme 5 --crf 13.7 --qcomp 0.72 --b-pyramid --merange 48 --weightp --weightb --rd 4 --psy-rd 2 --rdoq-level 2 --psy-rdoq 4 --sar 1:1 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "17_720.hevc" --csv-log-level 2 --csv "17_720.txt" --y4m -
+
+.\vaporPortable-r44\vspipe.exe -y 18.vpy - | x265-x64-v3.0-aMod -F 16 --pools "" --hevc-aq --qp-adaptation-range 2 --limit-modes --open-gop --cbqpoffs -2 --crqpoffs -2 --no-rskip --no-tskip --keyint 240 --no-cutree --ref 4 --bframes 9 --bframe-bias 0 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 0.86 --deblock 1:-1 --tu-intra-depth 2 --tu-inter-depth 2 --me 2 --wpp --subme 5 --crf 14.8 --qcomp 0.72 --b-pyramid --merange 48 --weightp --weightb --rd 4 --psy-rd 2 --rdoq-level 2 --psy-rdoq 4 --sar 1:1 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "18.hevc" --csv-log-level 2 --csv "18.txt" --y4m -
+
+.\vaporPortable-r44\vspipe.exe -y 18_720.vpy - | x265-x64-v3.0-aMod -F 16 --pools "" --hevc-aq --qp-adaptation-range 2 --limit-modes --open-gop --cbqpoffs -2 --crqpoffs -2 --no-rskip --no-tskip --keyint 240 --no-cutree --ref 6 --bframes 9 --bframe-bias 0 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 0.86 --deblock 1:-1 --tu-intra-depth 2 --tu-inter-depth 2 --me 2 --wpp --subme 5 --crf 13.7 --qcomp 0.72 --b-pyramid --merange 48 --weightp --weightb --rd 4 --psy-rd 2 --rdoq-level 2 --psy-rdoq 4 --sar 1:1 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "18_720.hevc" --csv-log-level 2 --csv "18_720.txt" --y4m -
+
+.\vaporPortable-r44\vspipe.exe -y 19.vpy - | x265-x64-v3.0-aMod -F 16 --pools "" --hevc-aq --qp-adaptation-range 2 --limit-modes --open-gop --cbqpoffs -2 --crqpoffs -2 --no-rskip --no-tskip --keyint 240 --no-cutree --ref 4 --bframes 9 --bframe-bias 0 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 0.86 --deblock 1:-1 --tu-intra-depth 2 --tu-inter-depth 2 --me 2 --wpp --subme 5 --crf 14.8 --qcomp 0.72 --b-pyramid --merange 48 --weightp --weightb --rd 4 --psy-rd 2 --rdoq-level 2 --psy-rdoq 4 --sar 1:1 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "19.hevc" --csv-log-level 2 --csv "19.txt" --y4m -
+
+.\vaporPortable-r44\vspipe.exe -y 19_720.vpy - | x265-x64-v3.0-aMod -F 16 --pools "" --hevc-aq --qp-adaptation-range 2 --limit-modes --open-gop --cbqpoffs -2 --crqpoffs -2 --no-rskip --no-tskip --keyint 240 --no-cutree --ref 6 --bframes 9 --bframe-bias 0 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 0.86 --deblock 1:-1 --tu-intra-depth 2 --tu-inter-depth 2 --me 2 --wpp --subme 5 --crf 13.7 --qcomp 0.72 --b-pyramid --merange 48 --weightp --weightb --rd 4 --psy-rd 2 --rdoq-level 2 --psy-rdoq 4 --sar 1:1 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "19_720.hevc" --csv-log-level 2 --csv "19_720.txt" --y4m -
+
+.\vaporPortable-r44\vspipe.exe -y 20.vpy - | x265-x64-v3.0-aMod -F 16 --pools "" --hevc-aq --qp-adaptation-range 2 --limit-modes --open-gop --cbqpoffs -2 --crqpoffs -2 --no-rskip --no-tskip --keyint 240 --no-cutree --ref 4 --bframes 9 --bframe-bias 0 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 0.86 --deblock 1:-1 --tu-intra-depth 2 --tu-inter-depth 2 --me 2 --wpp --subme 5 --crf 14.8 --qcomp 0.72 --b-pyramid --merange 48 --weightp --weightb --rd 4 --psy-rd 2 --rdoq-level 2 --psy-rdoq 4 --sar 1:1 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "20.hevc" --csv-log-level 2 --csv "20.txt" --y4m -
+
+.\vaporPortable-r44\vspipe.exe -y 20_720.vpy - | x265-x64-v3.0-aMod -F 16 --pools "" --hevc-aq --qp-adaptation-range 2 --limit-modes --open-gop --cbqpoffs -2 --crqpoffs -2 --no-rskip --no-tskip --keyint 240 --no-cutree --ref 6 --bframes 9 --bframe-bias 0 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 0.86 --deblock 1:-1 --tu-intra-depth 2 --tu-inter-depth 2 --me 2 --wpp --subme 5 --crf 13.7 --qcomp 0.72 --b-pyramid --merange 48 --weightp --weightb --rd 4 --psy-rd 2 --rdoq-level 2 --psy-rdoq 4 --sar 1:1 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "20_720.hevc" --csv-log-level 2 --csv "20_720.txt" --y4m -
+
+.\vaporPortable-r44\vspipe.exe -y 21.vpy - | x265-x64-v3.0-aMod -F 16 --pools "" --hevc-aq --qp-adaptation-range 2 --limit-modes --open-gop --cbqpoffs -2 --crqpoffs -2 --no-rskip --no-tskip --keyint 240 --no-cutree --ref 4 --bframes 9 --bframe-bias 0 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 0.86 --deblock 1:-1 --tu-intra-depth 2 --tu-inter-depth 2 --me 2 --wpp --subme 5 --crf 14.8 --qcomp 0.72 --b-pyramid --merange 48 --weightp --weightb --rd 4 --psy-rd 2 --rdoq-level 2 --psy-rdoq 4 --sar 1:1 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "21.hevc" --csv-log-level 2 --csv "21.txt" --y4m -
+
+.\vaporPortable-r44\vspipe.exe -y 21_720.vpy - | x265-x64-v3.0-aMod -F 16 --pools "" --hevc-aq --qp-adaptation-range 2 --limit-modes --open-gop --cbqpoffs -2 --crqpoffs -2 --no-rskip --no-tskip --keyint 240 --no-cutree --ref 6 --bframes 9 --bframe-bias 0 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 0.86 --deblock 1:-1 --tu-intra-depth 2 --tu-inter-depth 2 --me 2 --wpp --subme 5 --crf 13.7 --qcomp 0.72 --b-pyramid --merange 48 --weightp --weightb --rd 4 --psy-rd 2 --rdoq-level 2 --psy-rdoq 4 --sar 1:1 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "21_720.hevc" --csv-log-level 2 --csv "21_720.txt" --y4m -
+
+
+
+.\vaporPortable-r44\vspipe.exe -y 18.vpy - | x265-x64-v3.0-aMod -F 16 --pools "" --hevc-aq --qp-adaptation-range 2 --limit-modes --open-gop --cbqpoffs -2 --crqpoffs -2 --no-rskip --no-tskip --keyint 240 --no-cutree --ref 4 --bframes 9 --bframe-bias 0 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 0.86 --deblock 1:-1 --tu-intra-depth 2 --tu-inter-depth 2 --me 2 --wpp --subme 5 --crf 14.8 --qcomp 0.72 --b-pyramid --merange 48 --weightp --weightb --rd 4 --psy-rd 2 --rdoq-level 2 --psy-rdoq 4 --sar 1:1 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "18.hevc" --csv-log-level 2 --csv "18.txt" --y4m -
+
+.\vaporPortable-r44\vspipe.exe -y 18_720.vpy - | x265-x64-v3.0-aMod -F 16 --pools "" --hevc-aq --qp-adaptation-range 2 --limit-modes --open-gop --cbqpoffs -2 --crqpoffs -2 --no-rskip --no-tskip --keyint 240 --no-cutree --ref 6 --bframes 9 --bframe-bias 0 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 0.86 --deblock 1:-1 --tu-intra-depth 2 --tu-inter-depth 2 --me 2 --wpp --subme 5 --crf 13.7 --qcomp 0.72 --b-pyramid --merange 48 --weightp --weightb --rd 4 --psy-rd 2 --rdoq-level 2 --psy-rdoq 4 --sar 1:1 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "18_720.hevc" --csv-log-level 2 --csv "18_720.txt" --y4m -
+
+
+
+.\vaporPortable-r44\vspipe.exe -y 17_720.vpy - | x265-x64-v3.0-aMod -F 16 --pools "" --hevc-aq --qp-adaptation-range 2 --limit-modes --open-gop --cbqpoffs -2 --crqpoffs -2 --no-rskip --no-tskip --keyint 240 --no-cutree --ref 6 --bframes 9 --bframe-bias 0 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 0.86 --deblock 1:-1 --tu-intra-depth 2 --tu-inter-depth 2 --me 2 --wpp --subme 5 --crf 13.7 --qcomp 0.72 --b-pyramid --merange 48 --weightp --weightb --rd 4 --psy-rd 2 --rdoq-level 2 --psy-rdoq 4 --sar 1:1 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "17_720.hevc" --csv-log-level 2 --csv "17_720.txt" --y4m -
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pause
