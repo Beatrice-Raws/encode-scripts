478961c519f3c56eb0ba1a50d0d1b91386e6a4ed
Endill <brainvlad@gmail.com>
2021-02-20 22:43:40 +0300

    Add sources for Saenai Heroine no Sodatekata

---
+++ Saenai Heroine no Sodatekata Fine/encod_vpy2.bat
@@ -0,0 +1,18 @@
+.\VapourSynth64-Portable-R50\vspipe.exe -y 03.vpy - | x265-10b --pme --pmode -F 16 --qp-adaptation-range 2 --open-gop --cbqpoffs -2 --crqpoffs -2 --no-early-skip --rskip 1 --keyint 240 --ref 4 --bframes 8 --b-pyramid --b-adapt 2 --no-sao --no-cutree --no-sao-non-deblock --aq-mode 3 --no-strong-intra-smoothing --aq-strength 1.03 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 2 --me 3 --wpp --subme 7 --crf 15 --b-pyramid --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 4 --refine-mv 3 --analyze-src-pics --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "01.hevc" --csv-log-level 0 --csv "00.txt" --y4m -
+
+.\VapourSynth64-Portable-R50\vspipe.exe -y 00003.vpy - | x265-10b --pme --pmode -F 16 --qp-adaptation-range 2 --open-gop --cbqpoffs -2 --crqpoffs -2 --no-early-skip --rskip 1 --keyint 240 --ref 4 --bframes 8 --b-pyramid --b-adapt 2 --no-sao --no-cutree --no-sao-non-deblock --aq-mode 3 --no-strong-intra-smoothing --aq-strength 1.03 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 2 --me 3 --wpp --subme 7 --crf 15 --b-pyramid --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 4 --refine-mv 3 --analyze-src-pics --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00003.hevc" --csv-log-level 0 --csv "00.txt" --y4m -
+
+.\VapourSynth64-Portable-R50\vspipe.exe -y 00004.vpy - | x265-10b --pme --pmode -F 16 --qp-adaptation-range 2 --open-gop --cbqpoffs -2 --crqpoffs -2 --no-early-skip --rskip 1 --keyint 240 --ref 4 --bframes 8 --b-pyramid --b-adapt 2 --no-sao --no-cutree --no-sao-non-deblock --aq-mode 3 --no-strong-intra-smoothing --aq-strength 1.03 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 2 --me 3 --wpp --subme 7 --crf 15 --b-pyramid --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 4 --refine-mv 3 --analyze-src-pics --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00004.hevc" --csv-log-level 0 --csv "00.txt" --y4m -
+
+.\VapourSynth64-Portable-R50\vspipe.exe -y 00005.vpy - | x265-10b --pme --pmode -F 16 --qp-adaptation-range 2 --open-gop --cbqpoffs -2 --crqpoffs -2 --no-early-skip --rskip 1 --keyint 240 --ref 4 --bframes 8 --b-pyramid --b-adapt 2 --no-sao --no-cutree --no-sao-non-deblock --aq-mode 3 --no-strong-intra-smoothing --aq-strength 1.03 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 2 --me 3 --wpp --subme 7 --crf 15 --b-pyramid --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 4 --refine-mv 3 --analyze-src-pics --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00005.hevc" --csv-log-level 0 --csv "00.txt" --y4m -
+
+.\VapourSynth64-Portable-R50\vspipe.exe -y 00006.vpy - | x265-10b --pme --pmode -F 16 --qp-adaptation-range 2 --open-gop --cbqpoffs -2 --crqpoffs -2 --no-early-skip --rskip 1 --keyint 240 --ref 4 --bframes 8 --b-pyramid --b-adapt 2 --no-sao --no-cutree --no-sao-non-deblock --aq-mode 3 --no-strong-intra-smoothing --aq-strength 1.03 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 2 --me 3 --wpp --subme 7 --crf 15 --b-pyramid --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 4 --refine-mv 3 --analyze-src-pics --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00006.hevc" --csv-log-level 0 --csv "00.txt" --y4m -
+
+
+
+
+
+
+
+
+pause
---
+++ Saenai Heroine no Sodatekata Flat/encod_vpy2.bat
@@ -0,0 +1,14 @@
+@echo off
+FOR %%I IN (11) DO (
+.\VapourSynth64-Portable-R50\vspipe.exe -y %%I.vpy - | x265-10b --pme --pmode -F 16 --qp-adaptation-range 2 --open-gop --cbqpoffs -2 --crqpoffs -2 --no-early-skip --rskip 1 --keyint 240 --no-cutree --ref 4 --bframes 8 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --no-strong-intra-smoothing --aq-strength 1.03 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 2 --me 3 --wpp --subme 7 --crf 15.5 --b-pyramid --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 4 --refine-mv 3 --analyze-src-pics --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "%%I.hevc" --csv-log-level 0 --csv "%%I.txt" --y4m -
+)
+
+
+
+
+
+
+
+
+
+pause
