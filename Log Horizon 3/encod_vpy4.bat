12a7ce847b4cbb107b4c628473ba4aa39becd007
Endill <brainvlad@gmail.com>
2021-07-30 23:06:33 +0300

    Add sources for Log Horizon 3

---
+++ Log Horizon 3/encod_vpy4.bat
@@ -0,0 +1,21 @@
+.\VapourSynth64-Portable-R50\vspipe.exe -y 08.vpy - | x265-x64-v3.5+20-aMod-gcc10.2.1-opt-ivybridge --pme --no-strong-intra-smoothing --pmode -F 16 --qp-adaptation-range 2 --open-gop --cbqpoffs -2 --crqpoffs -2 --no-early-skip --rskip 0 --keyint 240 --ref 4 --bframes 8 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 1.03 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 2 --me 3 --wpp --subme 7 --crf 16 --b-pyramid --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 4 --psy-rd 2 --refine-mv 3 --analyze-src-pics --temporal-mvp --max-merge 5 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "08.hevc" --csv-log-level 0 --csv "16.txt" --y4m -
+
+.\VapourSynth64-Portable-R50\vspipe.exe -y 09.vpy - | x265-x64-v3.5+20-aMod-gcc10.2.1-opt-ivybridge --pme --no-strong-intra-smoothing --pmode -F 16 --qp-adaptation-range 2 --open-gop --cbqpoffs -2 --crqpoffs -2 --no-early-skip --rskip 0 --keyint 240 --ref 4 --bframes 8 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 1.03 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 2 --me 3 --wpp --subme 7 --crf 16 --b-pyramid --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 4 --psy-rd 2 --refine-mv 3 --analyze-src-pics --temporal-mvp --max-merge 5 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "09.hevc" --csv-log-level 0 --csv "16.txt" --y4m -
+
+.\VapourSynth64-Portable-R50\vspipe.exe -y 10.vpy - | x265-x64-v3.5+20-aMod-gcc10.2.1-opt-ivybridge --pme --no-strong-intra-smoothing --pmode -F 16 --qp-adaptation-range 2 --open-gop --cbqpoffs -2 --crqpoffs -2 --no-early-skip --rskip 0 --keyint 240 --ref 4 --bframes 8 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 1.03 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 2 --me 3 --wpp --subme 7 --crf 16 --b-pyramid --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 4 --psy-rd 2 --refine-mv 3 --analyze-src-pics --temporal-mvp --max-merge 5 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "10.hevc" --csv-log-level 0 --csv "16.txt" --y4m -
+
+.\VapourSynth64-Portable-R50\vspipe.exe -y 11.vpy - | x265-x64-v3.5+20-aMod-gcc10.2.1-opt-ivybridge --pme --no-strong-intra-smoothing --pmode -F 16 --qp-adaptation-range 2 --open-gop --cbqpoffs -2 --crqpoffs -2 --no-early-skip --rskip 0 --keyint 240 --ref 4 --bframes 8 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 1.03 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 2 --me 3 --wpp --subme 7 --crf 16 --b-pyramid --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 4 --psy-rd 2 --refine-mv 3 --analyze-src-pics --temporal-mvp --max-merge 5 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "11.hevc" --csv-log-level 0 --csv "16.txt" --y4m -
+
+.\VapourSynth64-Portable-R50\vspipe.exe -y 12.vpy - | x265-x64-v3.5+20-aMod-gcc10.2.1-opt-ivybridge --pme --no-strong-intra-smoothing --pmode -F 16 --qp-adaptation-range 2 --open-gop --cbqpoffs -2 --crqpoffs -2 --no-early-skip --rskip 0 --keyint 240 --ref 4 --bframes 8 --b-pyramid --b-adapt 2 --no-sao --no-sao-non-deblock --aq-mode 3 --aq-strength 1.03 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 2 --me 3 --wpp --subme 7 --crf 16 --b-pyramid --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 4 --psy-rd 2 --refine-mv 3 --analyze-src-pics --temporal-mvp --max-merge 5 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "12.hevc" --csv-log-level 0 --csv "16.txt" --y4m -
+
+
+
+
+
+
+
+
+
+pause
+
+
