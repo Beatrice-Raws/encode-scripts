ba63375d7b463f494e83b347d5bb1b3773a70de8
Endill <brainvlad@gmail.com>
2021-11-10 23:56:54 +0300

    Add sources for M3 - Sono Kuroki Hagane

---
+++ M3 - Sono Kuroki Hagane/encod — копия.bat
@@ -0,0 +1,4 @@
+avs2yuv64 -nstdr NCED.avs -o - | x264-10bit-x64-2597 --level 5.0 --crf 15 --keyint 240 --deblock 1:-1  --bframes 9 --b-adapt 2 --ref 4 --deadzone-inter 21 --deadzone-intra 11 --qcomp 0.72 --aq-strength 0.85 --merange 32 --me umh --subme 9 --trellis 2 --direct spatial --no-mbtree --sar 1:1 --threads auto --colormatrix "bt709" --colorprim "bt709" --transfer "bt709" --output NCED.264 --log-file NCED.enc.log --demuxer y4m   -
+
+avs2yuv64 -nstdr NCOP.avs -o - | x264-10bit-x64-2597 --level 5.0 --crf 15 --keyint 240 --deblock 1:-1  --bframes 9 --b-adapt 2 --ref 4 --deadzone-inter 21 --deadzone-intra 11 --qcomp 0.72 --aq-strength 0.85 --merange 32 --me umh --subme 9 --trellis 2 --direct spatial --no-mbtree --sar 1:1 --threads auto --colormatrix "bt709" --colorprim "bt709" --transfer "bt709" --output NCOP.264 --log-file NCOP.enc.log --demuxer y4m   -
+
---
+++ M3 - Sono Kuroki Hagane/yuv.bat
@@ -0,0 +1,3 @@
+avs2yuv64 NCOP2.avs -o NCOP2.yuv
+avs2yuv64 NCED2.avs -o NCED2.yuv
+
