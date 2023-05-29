3f7b63342f481a12cd7a54b37098a87bddfdcf19
DJATOM <djatom@beatrice-raws.org>
2023-05-29 21:37:30 +0300

    Add sources for Evangelion 3.0+1.0 Thrice Upon a Time

---
+++ Evangelion 3.0+1.0 Thrice Upon a Time/1632p/encod2.bat
@@ -0,0 +1,24 @@
+"C:\Program Files\VapourSynth\core\vspipe.exe" -c y4m H:\EVA4\2160\Film_2160.vpy - | H:\EVA4\2160\x265-x64-v3.5+67-aMod-gcc12.2.1-opt-sandybridge --zones 1,3346,b=1.4 --limit-modes --tskip-fast --rskip-edge-threshold 2 --splitrd-skip --rdpenalty 1 --sao --selective-sao 1 --limit-sao --sao-non-deblock --rect --qp-adaptation-range 4 --amp --no-cu-lossless --no-limit-modes --limit-refs 2 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --tskip --rskip 2 --keyint 240 --ref 4 --bframes 10 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.03 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 3 --me 3 --wpp --subme 7 --crf 14 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.65 --info --colorprim bt2020 --transfer bt2020-10 --colormatrix bt2020nc --output "H:\EVA4\2160\Film_2160.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pause
+
---
+++ Evangelion 3.0+1.0 Thrice Upon a Time/816p/encod2.bat
@@ -0,0 +1,14 @@
+"C:\Program Files\VapourSynth\core\vspipe.exe" -c y4m H:\EVA4\2160\Film.vpy - | H:\EVA4\2160\x265-x64-v3.5+67-aMod-gcc12.2.1-opt-sandybridge --zones 1,3346,b=1.6/3347,32364,b=1.4/32365,33865,b=1.2/33866,177023,b=1.4/177024,177274,b=1.1/177275,177278,b=1.4/177279,177373,b=1.1/177374,179114,b=1.4/179115,179231,b=1.1/179232,179267,b=1.4/179268,179529,b=1.1/179530,179685,b=1.4/179686,179874,b=1.1/179875,180307,b=1.4/180308,180730,b=1.1/180731,180895,b=1.4/180896,181761,b=1.1/181762,181815,b=1.4/181816,182886,b=1.1/182887,182971,b=1.4/182972,183340,b=1.1/183341,209881,b=1.4/209882,222743,b=1.1 --limit-modes --tskip-fast --rskip-edge-threshold 2 --splitrd-skip --rdpenalty 1 --sao --selective-sao 1 --limit-sao --sao-non-deblock --rect --qp-adaptation-range 4 --amp --no-cu-lossless --no-limit-modes --limit-refs 2 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --tskip --rskip 2 --keyint 240 --ref 4 --bframes 14 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.02 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 3 --me 3 --wpp --subme 7 --crf 14 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rd 1.7 --psy-rdoq 2.3 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "H:\EVA4\2160\Film.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+
+
+
+
+
+
+
+
+
+
+pause
+
