c70b8b698c61beaf1137dd4f7ba56bbd96f08a2a
DJATOM <djatom@beatrice-raws.org>
2023-06-02 10:45:10 +0300

    Add sources for Natsu e no Tunnel, Sayonara no Deguchi

---
+++ Natsu e no Tunnel, Sayonara no Deguchi/encod2.bat
@@ -0,0 +1,39 @@
+"C:\Program Files\VapourSynth\core\vspipe.exe" -c y4m H:\Natsu.Eno.Tunnel.Sayonara.No.Deguchi.2023.COMPLETE.BLURAY-KoushinRip\NATSUTON\BDMV\STREAM\Film_final.vpy - | H:\EVA4\2160\x265-x64-v3.5+67-aMod-gcc12.2.1-opt-sandybridge --zones 1,112919,b=1.4/112920,118919,b=1.1 --tskip-fast --splitrd-skip --no-sao --rect --qp-adaptation-range 4 --amp --cu-lossless --no-limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --early-skip --tskip --rskip 0 --keyint 240 --ref 4 --bframes 10 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.00 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 3 --me 3 --wpp --subme 7 --crf 14 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rd 1.9 --psy-rdoq 2.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.70 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "H:\Natsu.Eno.Tunnel.Sayonara.No.Deguchi.2023.COMPLETE.BLURAY-KoushinRip\NATSUTON\BDMV\STREAM\Film_final.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+
+"C:\Program Files\VapourSynth\core\vspipe.exe" -c y4m H:\Natsu.Eno.Tunnel.Sayonara.No.Deguchi.2023.COMPLETE.BLURAY-KoushinRip\NATSUTON\BDMV\STREAM\00001.vpy - | H:\EVA4\2160\x265-x64-v3.5+67-aMod-gcc12.2.1-opt-sandybridge --tskip-fast --splitrd-skip --no-sao --rect --qp-adaptation-range 4 --amp --cu-lossless --no-limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --early-skip --tskip --rskip 0 --keyint 240 --ref 4 --bframes 10 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.00 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 3 --me 3 --wpp --subme 7 --crf 14 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rd 1.9 --psy-rdoq 2.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.70 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "H:\Natsu.Eno.Tunnel.Sayonara.No.Deguchi.2023.COMPLETE.BLURAY-KoushinRip\NATSUTON\BDMV\STREAM\00001.hevc" --csv-log-level 0 --csv "00001.txt" --y4m -
+
+"C:\Program Files\VapourSynth\core\vspipe.exe" -c y4m H:\Natsu.Eno.Tunnel.Sayonara.No.Deguchi.2023.COMPLETE.BLURAY-KoushinRip\NATSUTON\BDMV\STREAM\00002.vpy - | H:\EVA4\2160\x265-x64-v3.5+67-aMod-gcc12.2.1-opt-sandybridge --tskip-fast --splitrd-skip --no-sao --rect --qp-adaptation-range 4 --amp --cu-lossless --no-limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --early-skip --tskip --rskip 0 --keyint 240 --ref 4 --bframes 10 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.00 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 3 --me 3 --wpp --subme 7 --crf 14 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rd 1.9 --psy-rdoq 2.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.70 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "H:\Natsu.Eno.Tunnel.Sayonara.No.Deguchi.2023.COMPLETE.BLURAY-KoushinRip\NATSUTON\BDMV\STREAM\00002.hevc" --csv-log-level 0 --csv "00002.txt" --y4m -
+
+"C:\Program Files\VapourSynth\core\vspipe.exe" -c y4m H:\Natsu.Eno.Tunnel.Sayonara.No.Deguchi.2023.COMPLETE.BLURAY-KoushinRip\NATSUTON\BDMV\STREAM\00003.vpy - | H:\EVA4\2160\x265-x64-v3.5+67-aMod-gcc12.2.1-opt-sandybridge --tskip-fast --splitrd-skip --no-sao --rect --qp-adaptation-range 4 --amp --cu-lossless --no-limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --early-skip --tskip --rskip 0 --keyint 240 --ref 4 --bframes 10 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.00 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 3 --me 3 --wpp --subme 7 --crf 14 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rd 1.9 --psy-rdoq 2.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.70 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "H:\Natsu.Eno.Tunnel.Sayonara.No.Deguchi.2023.COMPLETE.BLURAY-KoushinRip\NATSUTON\BDMV\STREAM\00003.hevc" --csv-log-level 0 --csv "00003.txt" --y4m -
+
+"C:\Program Files\VapourSynth\core\vspipe.exe" -c y4m H:\Natsu.Eno.Tunnel.Sayonara.No.Deguchi.2023.COMPLETE.BLURAY-KoushinRip\NATSUTON\BDMV\STREAM\00004.vpy - | H:\EVA4\2160\x265-x64-v3.5+67-aMod-gcc12.2.1-opt-sandybridge --tskip-fast --splitrd-skip --no-sao --rect --qp-adaptation-range 4 --amp --cu-lossless --no-limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --early-skip --tskip --rskip 0 --keyint 240 --ref 4 --bframes 10 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.00 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 3 --me 3 --wpp --subme 7 --crf 14 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rd 1.9 --psy-rdoq 2.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.70 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "H:\Natsu.Eno.Tunnel.Sayonara.No.Deguchi.2023.COMPLETE.BLURAY-KoushinRip\NATSUTON\BDMV\STREAM\00004.hevc" --csv-log-level 0 --csv "00004.txt" --y4m -
+
+"C:\Program Files\VapourSynth\core\vspipe.exe" -c y4m H:\Natsu.Eno.Tunnel.Sayonara.No.Deguchi.2023.COMPLETE.BLURAY-KoushinRip\NATSUTON\BDMV\STREAM\00008.vpy - | H:\EVA4\2160\x265-x64-v3.5+67-aMod-gcc12.2.1-opt-sandybridge --tskip-fast --splitrd-skip --no-sao --rect --qp-adaptation-range 4 --amp --cu-lossless --no-limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --early-skip --tskip --rskip 0 --keyint 240 --ref 4 --bframes 10 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.00 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 3 --me 3 --wpp --subme 7 --crf 14 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rd 1.9 --psy-rdoq 2.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.70 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "H:\Natsu.Eno.Tunnel.Sayonara.No.Deguchi.2023.COMPLETE.BLURAY-KoushinRip\NATSUTON\BDMV\STREAM\00008.hevc" --csv-log-level 0 --csv "00008.txt" --y4m -
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pause
+
