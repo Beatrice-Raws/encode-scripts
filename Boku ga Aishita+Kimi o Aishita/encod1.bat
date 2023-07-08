9c5401ed3d22afcc8ffb898e6d11a62f3e0fe699
DJATOM <djatom@beatrice-raws.org>
2023-07-08 21:35:40 +0300

    Add sources for Boku ga Aishita+Kimi o Aishita

---
+++ Boku ga Aishita+Kimi o Aishita/encod1.bat
@@ -0,0 +1,70 @@
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 01.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --zones 1,625,b=0.7 --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 2 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 240 --ref 4 --bframes 14 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 2.6 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "Film1.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+
+
+
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00009.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 240 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00009.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00011.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 240 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00011.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00012.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 240 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00012.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00014.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 240 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00014.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00015.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 240 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00015.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00017.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 240 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00017.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00018.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 240 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00018.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00020.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 300 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00020.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00022.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 300 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00022.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00023.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 300 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00023.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pause
+
---
+++ Boku ga Aishita+Kimi o Aishita/encod2.bat
@@ -0,0 +1,39 @@
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 02.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 240 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 14 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "film2.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00010.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 240 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00010.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00013.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 240 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00013.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00016.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 240 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00016.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00019.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 240 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00019.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00021.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 300 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00021.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+.\VapourSynth64-Portable-R55-API4-RC1\vspipe.exe -c y4m 00024.vpy - | x265-x64-v3.5+131-aMod-gcc13.1.0-opt-sandybridge --ctu 32 --max-tu-size 16 --tskip-fast --no-splitrd-skip --sao --selective-sao 4 --limit-sao --no-sao-non-deblock --rect --qp-adaptation-range 4 --amp --cu-lossless --limit-modes --limit-refs 0 --rc-lookahead 240 --no-hist-scenecut --no-strong-intra-smoothing --analyze-src-pics -F 16 --open-gop --cbqpoffs -2 --crqpoffs -2 --lookahead-slices 0 --no-early-skip --no-tskip --rskip 0 --keyint 300 --ref 4 --bframes 12 --b-pyramid --b-adapt 2 --aq-mode 3 --aq-strength 1.01 --deblock 1:-1 --tu-intra-depth 2 --refine-intra 4 --dynamic-refine --tu-inter-depth 4 --me 3 --wpp --subme 7 --crf 15 --weightp --weightb --rd 5 --rdoq-level 2 --psy-rdoq 3.0 --refine-mv 3 --temporal-mvp --max-merge 5 --qcomp 0.72 --info --colorprim bt709 --transfer bt709 --colormatrix bt709 --output "00024.hevc" --csv-log-level 0 --csv "Film.txt" --y4m -
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pause
+
